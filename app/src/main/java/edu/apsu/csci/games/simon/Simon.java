e77c64a501920217bd0f7e42c500445cba670380
Tyler Brown <brown.tc7@gmail.com>
2019-03-13 19:15:07 -0400

    flash and sound working at same time **in class**

--- app/src/main/java/edu/apsu/csci/games/simon/Simon.java
+++ app/src/main/java/edu/apsu/csci/games/simon/Simon.java
@@ -2,10 +2,13 @@ package edu.apsu.csci.games.simon;
 
 import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import java.util.Random;
+import java.util.Set;
 
 
 public class Simon {
@@ -15,15 +18,16 @@ public class Simon {
     private int current_round = 0;
     private int player_round = 0;
     private Random rand = new Random();
-/*    private Activity activity;
+    private Activity activity;
 
 
     Simon (Activity activity){
        this.activity = activity;
-    }*/
+    }
 
     //play next round
     public int[] playNext(){
+        Log.i("Simon Class", "Playing new round");
         simon_sequence[current_round] = rand.nextInt(3) + 1;
         current_round++;
         return simon_sequence;
@@ -76,16 +80,34 @@ public class Simon {
         return true;
     }
 
+    public void playSimonSequence(){
+        playNext();
+
+        for(int i = 0; i < current_round; i++){
+            Log.i("Simon Class", Integer.toString(simon_sequence[i]) + ", " + Integer.toString(current_round));
+            switch (simon_sequence[i]){
+                case 1:
+                    Log.i("Simon Class", "Performing Click 1 - Green");
+                    activity.findViewById(R.id.button_green).performClick();
+                case 2:
+                    activity.findViewById(R.id.button_red).performClick();
+                case 3:
+                    activity.findViewById(R.id.button_yellow).performClick();
+                case 4:
+                    activity.findViewById(R.id.button_blue).performClick();
+            }
+        }
+    }
 
 }
-class FlashButton extends AsyncTask<Integer, Integer, Void> {
+class GameAnamations extends AsyncTask<Integer, Integer, Void> {
 
     private Activity activity;
     private final static int ALPHA_MAX = 255;
     private final static int ALPHA_MIN = 32;
     private final static int FLASH_MS = 200; // is this an emulator issue; or should i not be updating the ui like this? (some flash slow, some flash fast, some not at all); however the click is being registered!
 
-    FlashButton (Activity activity){
+    GameAnamations (Activity activity){
         this.activity = activity;
     }
 
@@ -106,5 +128,119 @@ class FlashButton extends AsyncTask<Integer, Integer, Void> {
     @Override
     protected void onProgressUpdate(Integer... values) {
         activity.findViewById(values[0]).getBackground().setAlpha(values[1]);
+    }
+}
+
+//class GameSounds extends AsyncTask<Integer, Integer, Void>{
+//    private SoundPool soundPool; //variable for sounds
+//    private Set<Integer> soundsLoaded; //variable for sounds ArrayList
+//    private Activity activity;
+//
+//    GameSounds(Activity activity){
+//
+//        this.activity = activity;
+//
+//        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+//        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+//
+//        SoundPool.Builder spBuilder = new SoundPool.Builder();
+//        spBuilder.setAudioAttributes(attrBuilder.build());
+//        spBuilder.setMaxStreams(4);
+//        soundPool = spBuilder.build();
+//
+//        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+//            @Override
+//            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+//                if (status == 0) { // success
+//                    soundsLoaded.add(sampleId);
+//
+//                    Log.i("SOUND", "Sound loaded " + sampleId);
+//                } else {
+//                    Log.i("SOUND", "Error cannot load sound status = " + status);
+//                }
+//            }
+//        });
+//
+//    }
+//
+//    @Override
+//    protected Void doInBackground(Integer... values) {
+//        final int soundId_1 = soundPool.load(activity, R.raw.button1, 1);
+//        final int soundId_2 = soundPool.load(activity, R.raw.button2, 1);
+//        final int soundId_3 = soundPool.load(activity, R.raw.button3, 1);
+//        final int soundId_4 = soundPool.load(activity, R.raw.button4, 1);
+//        return null;
+//    }
+//
+//    protected void onProgressUpdate(int buttonId) {
+//        //if (soundsLoaded.contains(values[0])) {
+//            soundPool.play(buttonId, 12.0f, 12.0f, 0, 0, 1.0f);
+//        //}
+//    }
+//}
+
+
+class GameSounds {
+    private SoundPool soundPool; //variable for sounds
+    private Set<Integer> soundsLoaded; //variable for sounds ArrayList
+    private Activity activity;
+    private int blueId;
+    private int greenId;
+    private int redId;
+    private int yellowId;
+
+
+    GameSounds(Activity activity) {
+
+        this.activity = activity;
+
+        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+
+        SoundPool.Builder spBuilder = new SoundPool.Builder();
+        spBuilder.setAudioAttributes(attrBuilder.build());
+        spBuilder.setMaxStreams(8);
+        soundPool = spBuilder.build();
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if (status == 0) { // success
+                    //soundsLoaded.add(sampleId);
+
+                    Log.i("SOUND", "Sound loaded " + sampleId);
+                } else {
+                    Log.i("SOUND", "Error cannot load sound status = " + status);
+                }
+            }
+        });
+
+    }
+    public void loadSounds(){
+        blueId = soundPool.load(activity, R.raw.button1, 1);
+        redId = soundPool.load(activity, R.raw.button2, 1);
+        greenId = soundPool.load(activity, R.raw.button3, 1);
+        yellowId = soundPool.load(activity, R.raw.button4, 1);
+    }
+
+    public void playSound(int id){
+        int result = -1;
+        Log.i("SoundID", "blue " + Integer.toString(blueId));
+        Log.i("SoundID", "red " + Integer.toString(redId));
+        Log.i("SoundID", "gn " + Integer.toString(greenId));
+        Log.i("SoundID", "yl " + Integer.toString(yellowId));
+        if(id == R.id.button_blue){
+            result = soundPool.play(blueId, 12.0f, 12.0f, 0, 0, 1.0f);
+        }
+        else if (id == R.id.button_green) {
+            result = soundPool.play(greenId, 12.0f, 12.0f, 0, 0, 1.0f);
+        }
+        else if (id == R.id.button_red) {
+            result = soundPool.play(redId, 12.0f, 12.0f, 0, 0, 1.0f);
+        }
+        else if (id == R.id.button_yellow) {
+            result = soundPool.play(yellowId, 12.0f, 12.0f, 0, 0, 1.0f);
+        }
+        Log.i("SoundResult", Integer.toString(result));
     }
 }
--- app/src/main/java/edu/apsu/csci/games/simon/simon_react.java
+++ app/src/main/java/edu/apsu/csci/games/simon/simon_react.java
@@ -17,120 +17,141 @@ import java.util.Set;
 
 public class simon_react extends Activity {
 
-    private Simon simon = new Simon();
-    private SoundPool soundPool; //variable for sounds
-    private Set<Integer> soundsLoaded; //variable for sounds ArrayList
+    private Simon simon = new Simon(this);
+    //private SoundPool soundPool; //variable for sounds
+    //private Set<Integer> soundsLoaded; //variable for sounds ArrayList
 
+   private GameSounds gs = new GameSounds(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simon_react);
 
-        soundsLoaded = new HashSet<Integer>(); //ArrayList of sounds
+        //soundsLoaded = new HashSet<Integer>(); //ArrayList of sounds
 
-//        findViewById(R.id.button_blue).setOnClickListener(new buttonListener());
-//        findViewById(R.id.button_green).setOnClickListener(new buttonListener());
-//        findViewById(R.id.button_red).setOnClickListener(new buttonListener());
-//        findViewById(R.id.button_yellow).setOnClickListener(new buttonListener());
+
 
     }
-//    class buttonListener implements View.OnClickListener {
-//
-//        @Override
-//        public void onClick(View v) {
-//            FlashButton fb = new FlashButton(simon_react.this);
-//            fb.execute(v.getId());
-//
-//            Button button = (Button) v;
-//            int playerPick = Integer.parseInt(button.getText().toString());
-//
-//            simon.setPlayerPick(playerPick);
-//        }
-//    }
+    class buttonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            GameAnamations ga = new GameAnamations(simon_react.this);
+            //GameSounds gs = new GameSounds(simon_react.this);
+
+            ga.execute(v.getId());
+            //gs.execute(R.raw.button1);
+
+            gs.playSound(v.getId());
+
+
+            Button button = (Button) v;
+            int playerPick = Integer.parseInt(button.getText().toString());
+
+            simon.setPlayerPick(playerPick);
+        }
+    }
 
     //SoundPool code for button sounds
     @Override
     protected void onResume() {
         super.onResume();
 
-        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
-        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+        gs.loadSounds();
+        //gs.loadS(this);
 
-        SoundPool.Builder spBuilder = new SoundPool.Builder();
-        spBuilder.setAudioAttributes(attrBuilder.build());
-        spBuilder.setMaxStreams(80);
-        soundPool = spBuilder.build();
+        findViewById(R.id.button_blue).setOnClickListener(new buttonListener());
+        findViewById(R.id.button_green).setOnClickListener(new buttonListener());
+        findViewById(R.id.button_red).setOnClickListener(new buttonListener());
+        findViewById(R.id.button_yellow).setOnClickListener(new buttonListener());
+//        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+//        attrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+//
+//        SoundPool.Builder spBuilder = new SoundPool.Builder();
+//        spBuilder.setAudioAttributes(attrBuilder.build());
+//        spBuilder.setMaxStreams(4);
+//        soundPool = spBuilder.build();
+//
+//        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+//            @Override
+//            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+//                if (status == 0) { // success
+//                    soundsLoaded.add(sampleId);
+//
+//                    Log.i("SOUND", "Sound loaded " + sampleId);
+//                } else {
+//                    Log.i("SOUND", "Error cannot load sound status = " + status);
+//                }
+//            }
+//        });
+//
+//        final int blueId = soundPool.load(this, R.raw.button1, 1);
+//        findViewById(R.id.button_blue).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                playButton(blueId, v);
+//                Log.i("Simon OnClick", " blue loaded");
+//            }
+//        });
+//
+//        final int greenId = soundPool.load(this, R.raw.button2, 1);
+//        findViewById(R.id.button_green).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                playButton(greenId, v);
+//                Log.i("Simon OnClick", " green loaded");
+//            }
+//        });
+//
+//        final int redId = soundPool.load(this, R.raw.button3, 1);
+//        findViewById(R.id.button_red).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                playButton(redId, v);
+//                Log.i("Simon OnClick", " red loaded");
+//            }
+//        });
+//
+//        final int yellowId = soundPool.load(this, R.raw.button4, 1);
+//        findViewById(R.id.button_yellow).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                playButton(yellowId, v);
+//                Log.i("Simon OnClick", " yellow loaded");
+//            }
+//
+//        });
+//        Log.i("Sounds Loaded Size", Integer.toString(soundsLoaded.size()));
 
-        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                if (status == 0) { // success
-                    soundsLoaded.add(sampleId);
-                    Log.i("SOUND", "Sound loaded " + sampleId);
-                } else {
-                    Log.i("SOUND", "Error cannot load sound status = " + status);
-                }
-            }
-        });
-
-        final int blueId = soundPool.load(this, R.raw.button1, 1);
-        findViewById(R.id.button_blue).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playButton(blueId, v);
-            }
-        });
-
-        final int greenId = soundPool.load(this, R.raw.button2, 1);
-        findViewById(R.id.button_green).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playButton(greenId, v);
-            }
-        });
-
-        final int redId = soundPool.load(this, R.raw.button3, 1);
-        findViewById(R.id.button_red).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playButton(redId, v);
-            }
-        });
-
-        final int yellowId = soundPool.load(this, R.raw.button4, 1);
-        findViewById(R.id.button_yellow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playButton(yellowId, v);
-            }
-        });
-
+        //simon.playSimonSequence();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (soundPool != null) {
-            soundPool.release();
-            soundPool = null;
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        if (soundPool != null) {
+//            soundPool.release();
+//            soundPool = null;
+//
+//            soundsLoaded.clear();
+//        }
+//    }
 
-            soundsLoaded.clear();
-        }
-    }
-
-    private void playButton(int soundId, View buttonView) {
-        if (soundsLoaded.contains(soundId)) {
-            soundPool.play(soundId, 12.0f, 12.0f, 0, 0, 1.0f);
-        }
-        FlashButton fb = new FlashButton(simon_react.this);
-        fb.execute(buttonView.getId());
-
-        Button button = (Button) buttonView;
-        int playerPick = Integer.parseInt(button.getText().toString());
-
-        simon.setPlayerPick(playerPick);
-    }
+//    private void playButton(int soundId, View buttonView) {
+//        if (soundsLoaded.contains(soundId)) {
+//            soundPool.play(soundId, 12.0f, 12.0f, 0, 0, 1.0f);
+//        }
+//        FlashButton fb = new FlashButton(simon_react.this);
+//        fb.execute(buttonView.getId());
+//
+//        Button button = (Button) buttonView;
+//        int playerPick = Integer.parseInt(button.getText().toString());
+//
+//        simon.setPlayerPick(playerPick);
+//    }
+//
+//
 }
 
 
